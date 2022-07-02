43c53bfacf7241959678c154e77bf551f0011c55
Stefano Calzoni <stefanoclz@hotmail.it>
2022-07-02 13:09:28 +0200

    aggiunte migliorie menu

--- Menu.cs
+++ Menu.cs
@@ -29,6 +29,8 @@ namespace csharp_banca_oop
 
             if (selettore1 == 1)
             {
+                Console.Clear();
+
                 int userMenu = Menu.MenuUtente();
 
                 if (userMenu == 1)
@@ -38,16 +40,48 @@ namespace csharp_banca_oop
                     bank.NuovoCliente(nuovoCliente);
                     Console.WriteLine("Cliente aggiunto!");
                     bank.ListaClienti();
+                    Console.WriteLine("1. Torna a Menu Utente");
+                    Console.WriteLine("2. Torna a Home");
+                    int exit = Int32.Parse(Console.ReadLine());
+                    if (exit == 1)
+                    {
+                        MenuUtente();
+                    } else if(exit == 2)
+                    {
+                        Welcome(bank.nome);
+                    }
                 }
                 else if (userMenu == 2)
                 {
                     int modifier = bank.ListaClienti();
                     bank.NuovoCliente(bank.ModificaCliente(modifier));
                     bank.ListaClienti();
+                    Console.WriteLine("1. Torna a Menu Utente");
+                    Console.WriteLine("2. Torna a Home");
+                    int exit = Int32.Parse(Console.ReadLine());
+                    if (exit == 1)
+                    {
+                        MenuUtente();
+                    }
+                    else if (exit == 2)
+                    {
+                        Welcome(bank.nome);
+                    }
                 }
                 else if (userMenu == 3)
                 {
                     bank.RicercaCliente();
+                    Console.WriteLine("1. Torna a Menu Utente");
+                    Console.WriteLine("2. Torna a Home");
+                    int exit = Int32.Parse(Console.ReadLine());
+                    if (exit == 1)
+                    {
+                        MenuUtente();
+                    }
+                    else if (exit == 2)
+                    {
+                        Welcome(bank.nome);
+                    }
                 }
                 else if (userMenu == 4)
                 {
@@ -61,6 +95,8 @@ namespace csharp_banca_oop
             }
             else if (selettore1 == 2)
             {
+                Console.Clear();
+
                 int userLending = Menu.MenuPrestiti();
 
                 if (userLending == 1)
@@ -69,20 +105,47 @@ namespace csharp_banca_oop
                     bank.NuovoPrestito(nuovoPrestito);
                     Console.WriteLine("Presito aggiunto");
                     bank.ListaPrestiti();
+                    Console.WriteLine("1. Torna a Menu Prestiti");
+                    Console.WriteLine("2. Torna a Home");
+                    int exit = Int32.Parse(Console.ReadLine());
+                    if (exit == 1)
+                    {
+                        MenuPrestiti();
+                    }
+                    else if (exit == 2)
+                    {
+                        Welcome(bank.nome);
+                    }
                 }
                 else if (userLending == 2)
                 {
                     bank.RicercaPrestito();
+                    int exit = Int32.Parse(Console.ReadLine());
+                    if (exit == 1)
+                    {
+                        MenuPrestiti();
+                    }
+                    else if (exit == 2)
+                    {
+                        Welcome(bank.nome);
+                    }
                 }
                 else if (userLending == 3)
                 {
                     Welcome(bank.nome);
                 }
             }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Selezione errata, prego riprovare");
+                Welcome(bank.nome);
+            }
         }
 
         internal static int MenuPrestiti()
         {
+            Console.Clear();
             Console.WriteLine("Menu Prestiti:");
             Console.WriteLine("1. Aggiungi prestito");
             Console.WriteLine("2. Ricerca prestito tramite CF");
@@ -94,6 +157,7 @@ namespace csharp_banca_oop
 
         public static int MenuUtente()
         {
+            Console.Clear();
             Console.WriteLine("Menu Utente:");
             Console.WriteLine("1. Aggiungi nuovo utente");
             Console.WriteLine("2. Modifica utente");
